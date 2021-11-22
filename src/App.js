4b9e9163b02ea5bda6f160f00097f190848920eb
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-11-23 03:24:31 +0530

    firebase config file and very base html

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="app__body">
+        {/* sidebar */}
+        {/* chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCfw8SCT9VM6bQqK8OaP3_jw5t61ieKr9k",
+    authDomain: "whatsapp-clone-b3433.firebaseapp.com",
+    projectId: "whatsapp-clone-b3433",
+    storageBucket: "whatsapp-clone-b3433.appspot.com",
+    messagingSenderId: "381363679843",
+    appId: "1:381363679843:web:2a8f3ef6b8e4c6e1987e56",
+    measurementId: "G-8416XWY9CR"
+  };
