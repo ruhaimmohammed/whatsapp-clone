cfba4435b339f7adf77cdd4dc39153b16b1f6d26
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-11-24 23:31:46 +0530

    Getting room from DB

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Avatar, IconButton } from "@material-ui/core"
 import './Sidebar.css'
 import DonutLargeRoundedIcon from "@material-ui/icons/DonutLargeRounded"
@@ -6,8 +6,23 @@ import ChatIcon from "@material-ui/icons/Chat"
 import MoreVertIcon from "@material-ui/icons/MoreVert"
 import { SearchOutlined } from "@material-ui/icons"
 import SidebarChat from "./SidebarChat"
+import db from "./firebase"
 
 function Sidebar() {
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        db.collection('rooms').onSnapshot(snapshot =>
+            (
+               setRooms(snapshot.docs.map(doc => 
+                ({
+                    id:doc.id,
+                    data:doc.data(),
+                })
+                ))
+           ) )
+    }, [])
+
     return (
         <div className="sidebar">
             <div className="sidebar__header">
@@ -32,11 +47,9 @@ function Sidebar() {
             </div>
             <div className="sidebar__chats">
                 <SidebarChat  addNewChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-
+                {rooms.map(room => (
+                    <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+                ))};
 
             </div>
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
 const firebaseConfig = {
     apiKey: "AIzaSyCfw8SCT9VM6bQqK8OaP3_jw5t61ieKr9k",
     authDomain: "whatsapp-clone-b3433.firebaseapp.com",
@@ -8,3 +12,14 @@ const firebaseConfig = {
     appId: "1:381363679843:web:2a8f3ef6b8e4c6e1987e56",
     measurementId: "G-8416XWY9CR"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+
+  const auth = firebase.auth();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
