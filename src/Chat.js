e5fbe5a181d4e26e8a52d5d00f302b7528ed4a57
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-12-20 03:54:33 +0530

    Connection to DB on progress

--- src/Chat.js
+++ src/Chat.js
@@ -16,14 +16,24 @@ function Chat() {
 
     const [input, setInput] = useState("");
     const [seed, setSeed] = useState('');
-    const { roomId }  = useParams();
+    const { roomId } = useParams();
     const [roomName, setRoomName] = useState("");
+    const [messages, setMessages] = useState([]);
 
     useEffect(() => {
-        if(roomId){
+        if (roomId) {
             db.collection('rooms').doc(roomId).onSnapshot(snapshot => (
                 setRoomName(snapshot.data().name)
             ))
+
+            db.collection('rooms')
+                .doc(roomId)
+                .collection("messages")
+                .orderBy('timestamp', 'asc')
+                .onSnapshot((snapshot) =>
+                    setMessages(snapshot.doc.map((doc) => doc.
+                        data()))
+                )
         }
     }, [roomId])
 
@@ -42,45 +52,47 @@ function Chat() {
 
 
     return (
-            <div className="chat">
-                <div className="chat__header">
-                    <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
 
-                    <div className="chat__headerInfo">
-                        <h3>{ roomName }</h3>
-                        <p>Last seen ....</p>
-                    </div>
-
-                    <div className="chat__headerRight">
-                        <IconButton>
-                            <SearchOutlined />
-                        </IconButton>
-                        <IconButton>
-                            <AttachFile />
-                        </IconButton>
-                        <IconButton>
-                            <MoreVertIcon />
-                        </IconButton>
-                    </div>
+                <div className="chat__headerInfo">
+                    <h3>{roomName}</h3>
+                    <p>Last seen ....</p>
                 </div>
-                <div className="chat__body">
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                {messages.map(message => (
                     <p className={`chat__message ${true && 'chat__receiver'}`}>
-                        <span className="chat__name" style={{ color: `rgb(${red},${green},${blue})` }}>Ruhaim</span>
+                        <span className="chat__name" style={{ color: `rgb(${red},${green},${blue})` }}>{message.name}</span>
                         <div className="msg__container">
-                            <span className="chat__msg"> Hey Guys </span>
+                            <span className="chat__msg">{message.message}</span>
                             <span className="chat__timestamp">3:38 PM</span>
                         </div>
                     </p>
-                </div>
-                <div className="chat__footer">
-                    <InsertEmoticonIcon />
-                    <form>
-                        <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Type a message" />
-                        <button onClick={sendMessage} type="submit">Send a meassage</button>
-                    </form>
-                    <MicIcon />
-                </div>
+                ))}
             </div>
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Type a message" />
+                    <button onClick={sendMessage} type="submit">Send a meassage</button>
+                </form>
+                <MicIcon />
+            </div>
+        </div>
     )
 }
 
